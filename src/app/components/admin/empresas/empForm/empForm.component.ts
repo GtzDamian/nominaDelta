0f4b4e5eb85251fd0ad4786b1f88c765d1bd52b4
GtzDamian <gtz.damian@hotmail.com>
2021-05-18 10:52:59 -0500

    Se agrega vista de Login

---
+++ src/app/components/admin/empresas/empForm/empForm.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Empresa } from '../../../../models/dto/empresa';
+import { EmpresaService } from '../../../../models/services/empresa.service';
+import {Router, Routes, ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-empForm',
+  templateUrl: './empForm.component.html',
+  styleUrls: ['./empForm.component.css']
+})
+export class EmpFormComponent implements OnInit {
+
+  public empresa: Empresa = new Empresa();
+
+  constructor(
+    private empresaService: EmpresaService, 
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+    
+  ngOnInit(): void {
+    this.cargarEmpresa();
+  }
+
+  public create():void{
+    this.empresaService.create(this.empresa).subscribe(empresa => {
+      this.router.navigate(['/admin'])
+    })
+  }
+
+  cargarEmpresa(): void{
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if(id){
+        this.empresaService.getEmpresa(id).subscribe( (empresa) => this.empresa = empresa)
+      }
+    })
+  }
+
+  public update(): void{
+    this.empresaService.update(this.empresa).subscribe(empresa =>
+      {this.router.navigate(['/admin'])
+    })
+  }
+}
---
+++ src/app/components/admin/empresas/empresas.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Empresa } from '../../../models/dto/empresa';
+import Swal from 'sweetalert2';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { EmpresaService } from '../../../models/services/empresa.service';
+
+@Component({
+  selector: 'app-empresas',
+  templateUrl: './empresas.component.html',
+  styleUrls: ['./empresas.component.css']
+})
+export class EmpresasComponent implements OnInit {
+
+  titulo: string = "Delta Consultores, S. C.";
+
+  empresas!: Empresa[];
+  public empresa: Empresa = new Empresa();
+
+  constructor(private title: Title, private empresaService: EmpresaService, private router: Router) {
+    this.title.setTitle("Admin | Información Gerencial - Nómina");
+   }
+
+  ngOnInit(): void {
+    this.empresaService.getEmpresas().subscribe(
+      (empresas) => { this.empresas = empresas }
+    )
+  }
+
+  public delete(empresa: Empresa):void{
+    Swal.fire({
+      title: 'Borrar Empresa',
+      text: `¿Desea eliminar a ${empresa.nombre}?`,
+      icon: 'warning',
+      reverseButtons: true,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.empresaService.delete(empresa.id).subscribe(
+          response => {
+            Swal.fire('Eliminada', `${empresa.nombre} ha sido eliminada`,'success').
+            then(function(){ 
+              location.reload();
+            });
+          }
+        )
+      }
+    })
+  }
+
+
+}
